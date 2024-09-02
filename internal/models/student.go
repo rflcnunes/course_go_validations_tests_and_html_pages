e63107425a4379bdf80e662adcb07063df559187
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-09-01 23:41:24 -0300

    update student

--- internal/models/student.go
+++ internal/models/student.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type Student struct {
 	gorm.Model
 	Name string `json:"name" gorm:"column:name"`
-	CPF  string `json:"cpf" gorm:"column:cpf"`
-	RG   string `json:"rg" gorm:"column:rg"`
+	CPF  string `json:"cpf" gorm:"column:cpf;unique"`
+	RG   string `json:"rg" gorm:"column:rg;unique"`
 }
